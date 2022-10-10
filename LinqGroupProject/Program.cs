5d01fb9bcc093dd9ea778a171fffce0357326d7c
GitStudent2022 <torelgame@mail.ru>
2022-10-10 16:23:25 +0300

    Добавьте файлы проекта.

---
+++ LinqGroupProject/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace LinqGroupProject
+{
+    class Company
+    {
+        public string? Title { set; get; }
+    }
+    class Person
+    {
+        public string? Name { set; get; }
+        public Company? Company { set; get; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Company> companies = new List<Company>()
+            {
+                new Company{ Title = "Yandex" },
+                new Company{ Title = "Mail Group" },
+                new Company{ Title = "Google" },
+                new Company{ Title = "Microsoft" },
+            };
+            List<Person> persons = new List<Person>
+            {
+                new Person{ Name = "Bob", Company = companies[0] },
+                new Person{ Name = "Joe", Company = companies[1] },
+                new Person{ Name = "Tim", Company = companies[2] },
+                new Person{ Name = "Mike", Company = companies[3] },
+                new Person{ Name = "Bill", Company = companies[0] },
+                new Person{ Name = "Pill", Company = companies[2] },
+                new Person{ Name = "Sam", Company = companies[1] },
+                new Person{ Name = "Tom", Company = companies[1] },
+                new Person{ Name = "Jim", Company = companies[3] },
+            };
+            var personsComp = from p in persons
+                              group p by p.Company;
+            foreach(var comp in personsComp)
+            {
+                Console.WriteLine(comp.Key.Title);
+                foreach(var person in comp)
+                {
+                    Console.WriteLine($"\t{person.Name}");
+                }
+            }
+            Console.WriteLine(new String('-', 10));
+
+            var personsCompMethod = persons.GroupBy(p => p.Company);
+            foreach (var comp in personsComp)
+            {
+                Console.WriteLine(comp.Key.Title);
+                foreach (var person in comp)
+                {
+                    Console.WriteLine($"\t{person.Name}");
+                }
+            }
+            Console.WriteLine(new String('-', 10));
+
+
+            var personsCompNewOne = from p in persons
+                              group p by p.Company into temp
+                              select new { Title = temp.Key.Title, Count = temp.Count() };
+            foreach (var comp in personsCompNewOne)
+            {
+                Console.WriteLine($"{comp.Title} - {comp.Count}");
+            }
+            Console.WriteLine(new String('-', 10));
+        }
+    }
+}
