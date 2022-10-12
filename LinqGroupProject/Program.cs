807d5c95d95bfa52a6ee3084a402f19e35b5ceff
GitStudent2022 <torelgame@mail.ru>
2022-10-12 13:49:11 +0300

    12-10-22

--- LinqGroupProject/Program.cs
+++ LinqGroupProject/Program.cs
@@ -58,10 +58,58 @@
 
             var personsCompNewOne = from p in persons
                               group p by p.Company into temp
-                              select new { Title = temp.Key.Title, Count = temp.Count() };
+                              select new 
+                              { 
+                                  Title = temp.Key.Title, 
+                                  Count = temp.Count()
+                              };
             foreach (var comp in personsCompNewOne)
             {
                 Console.WriteLine($"{comp.Title} - {comp.Count}");
+            }
+            Console.WriteLine(new String('-', 10));
+
+            var personsCompNewTwo = persons.GroupBy(p => p.Company)
+                                           .Select(temp => new
+                                           {
+                                               Title = temp.Key.Title,
+                                               Count = temp.Count()
+                                           });
+            foreach (var comp in personsCompNewTwo)
+            {
+                Console.WriteLine($"{comp.Title} - {comp.Count}");
+            }
+            Console.WriteLine(new String('-', 10));
+
+            var compPersonsOne = from person in persons
+                              group person by person.Company into temp
+                              select new
+                              { 
+                                  Company = temp.Key.Title,
+                                  Count = temp.Count(),
+                                  Employes = from p in temp select p
+                              };
+            foreach(var comp in compPersonsOne)
+            {
+                Console.WriteLine($"{comp.Company} - {comp.Count}");
+                foreach (var person in comp.Employes)
+                    Console.WriteLine($"\t{person.Name}");
+            }
+            Console.WriteLine(new String('-', 10));
+
+            var compPersonsTwo = persons.GroupBy(p => p.Company)
+                                        .Select(temp => new
+                                        {
+                                            Company = temp.Key.Title,
+                                            Count = temp.Count(),
+                                            Employes = temp.Select(p => p)
+                                        });
+
+            foreach (var comp in compPersonsTwo)
+            {
+                Console.WriteLine($"{comp.Company} - {comp.Count}");
+                foreach (var person in comp.Employes)
+                    Console.WriteLine($"\t{person.Name}");
             }
             Console.WriteLine(new String('-', 10));
         }
